b28c135af3d08055954ba6db827df74d487aecfb
Mastan Patel <m.mastanpatel@gmail.com>
2022-05-09 07:53:26 +0530

    adde layout component

--- resourcealloc/src/App.js
+++ resourcealloc/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navi from './layouts/Navi';
+import Dashboard from './layouts/Dashboard';
+import {Grid} from 'semantic-ui-react';
+import Footer from './layouts/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App jobSeekerDetail'>
+        <Grid >
+          <Grid.Row computer={16} >
+            <Grid.Column computer={3} id="menu">
+              <Navi />
+            </Grid.Column>
+            <Grid.Column computer={13} >
+              <Dashboard />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Footer/>
     </div>
   );
 }
